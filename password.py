fd37e4414b3b6bf7fe37183cbdbf82322621d930
BILLY CHANG <ts04574870@gmail.com>
2020-07-25 01:38:19 +0900

    update new version

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 i = 3
 while i > 0:
+    i = i - 1
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功!')
         break
     else:
-        i = i - 1
-        print('登入失敗您還有' ,i ,'次機會')
+        print('登入失敗')
+        if i > 0:
+            print('還有', i ,'次機會')
+        else:
+            print('嘗試次數過多帳號已遭凍結')
      
